5f0950d266eda17631cc5228ded4c9e1b75305ad
arifsefrianto <arif.sefrianto@qerja.com>
2020-08-23 17:19:50 +0700

    Step 3 - Refactor

--- server/server_test.go
+++ server/server_test.go
@@ -106,6 +106,7 @@ func TestLeague(t *testing.T) {
 
 		assertStatus(t, response.Code, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
+		assertContentType(t, response, jsonContentType)
 
 		if response.Result().Header.Get("content-type") != "application/json" {
 			t.Errorf("response did not have content-type of application/json, git %v", response.Result().Header)
@@ -156,4 +157,13 @@ func assertLeague(t *testing.T, got, want []Player) {
 func newLeagueRequest() *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, "/league", nil)
 	return req
+}
+
+const jsonContentType = "application/json"
+
+func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
+	}
 }
